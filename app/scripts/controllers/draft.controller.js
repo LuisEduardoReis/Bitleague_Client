18d497c6d363cfcc2343bf794c6d249cb29e17f5
KR-Eddie <luiseddie.kr@gmail.com>
2016-05-04 20:59:47 +0100

    Simple pick functionality

--- app/scripts/controllers/draft.controller.js
+++ app/scripts/controllers/draft.controller.js
@@ -5,16 +5,5 @@ app.controller('DraftCtrl', function ($scope, $rootScope, srvAuth, srvDraft, $we
   srvDraft.draft.init('test');
   $scope.draft = srvDraft.draft;
 
-  /*var updateState = function () {
-    $scope.state = srvDraft.state;
-    $scope.user_list = srvDraft.user_list;
-    console.log($scope.state);
-    //$scope.$apply();
-  }*/
-  //updateState();
-  //srvDraft.registerStateObserver(updateState);
-
-
-
 
 });
--- app/scripts/services/draft.service.js
+++ app/scripts/services/draft.service.js
@@ -1,5 +1,5 @@
 
-app.factory('srvDraft', function (srvAuth, $websocket, $rootScope) {
+app.factory('srvDraft', function ($rootScope, srvAuth, $websocket, $http ) {
   var stateObservers = [];
 
   var draft = {
@@ -9,28 +9,65 @@ app.factory('srvDraft', function (srvAuth, $websocket, $rootScope) {
   draft.init = function (league_id) {
     if (draft.state != 'closed') return;
 
-    draft.state = 'connecting';
+    draft.state = 'loading';
     draft.league_id = league_id;
     draft.user_list = [];
     draft.players = [];
     draft.picks = [];
+    draft.players_left = {};
+    draft.picked_players = {};
+    draft.picknumber = 0;
 
-    draft.ws = $websocket.$new("ws://"+window.location.hostname+':'+$rootScope.SERVER_PORT+"/api/socket")
+    $http({
+      method: 'GET',
+      url: 'http://' + window.location.hostname +':'+ $rootScope.SERVER_PORT +'/api/players'
+    }).success(function (data) {
+      draft.players = data;
 
-    draft.ws.$on('$open', function() {
-      console.log('open');
-      draft.ws.$emit('init',{'Authorization': srvAuth.login.token, 'league_id': 'test'});
+      draft.state = 'connecting';
+      draft.ws = $websocket.$new("ws://"+window.location.hostname+':'+$rootScope.SERVER_PORT+"/api/socket")
+
+      draft.ws.$on('$open', function() {
+        console.log('open');
+        draft.ws.$emit('init',{'Authorization': srvAuth.login.token, 'league_id': 'test'});
+      });
+
+      draft.ws.$on('$message', function(res) {
+        draft.state = 'connected';
+        draft.notifyStateObservers();
+        console.log(res);
+
+        if(res.event == 'user_list') {
+          draft.user_list = res.data;
+        } else
+        if(res.event == 'pick_list') {
+          draft.picks = res.data;
+          draft.updatePlayersLeft();
+        } else
+        if(res.event == 'pick') {
+          draft.picks.push(res.data);
+          draft.updatePlayersLeft();
+        }
+
+        $rootScope.$apply();
+      });
     });
+  }
 
-    draft.ws.$on('$message', function(res) {
-      $rootScope.$apply();
-      draft.state = 'connected';
-      draft.notifyStateObservers();
-      console.log(res);
-      if(res.event == 'user_list') {
-        draft.user_list = res.data;
-      }
-    });
+  draft.updatePlayersLeft = function () {
+    draft.picked_players = {};
+    for(var i = 0; i < draft.picks.length; i++) {
+      draft.picked_players[draft.picks[i].player_id] = true;
+    }
+    draft.players_left = {};
+    for(var i = 0; i < draft.players.length; i++) {
+      if (draft.picked_players[draft.players[i]._id]) continue;
+      draft.players_left[draft.players[i]._id] = draft.players[i];
+    }
+  }
+
+  draft.pick = function (player_id) {
+    draft.ws.$emit('pick',{'player_id': player_id});
   }
 
   draft.registerStateObserver = function (callback) { stateObservers.push(callback); }
