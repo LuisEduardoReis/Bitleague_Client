ba6056f52728f92969d4091a016b0211d7b43682
Jorge Teixeira <ei12030@fe.up.pt>
2016-05-07 17:56:07 +0100

    draft page modified

--- app/scripts/controllers/draft.controller.js
+++ app/scripts/controllers/draft.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
 app.controller('DraftCtrl', function ($scope,  srvDraft, $stateParams) {
+  $scope.currentPage = 1;
+  $scope.pageSize = 8;
   
   srvDraft.draft.init($stateParams.league_id);
   $scope.draft = srvDraft.draft;
-
 
 });
