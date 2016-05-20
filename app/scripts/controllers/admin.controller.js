33c09e57f304e85162361af6aabf4af5179eca31
Luis <luiseddie.kr@gmail.com>
2016-05-20 20:55:14 +0100

    Finished admin page for now

--- app/scripts/controllers/admin.controller.js
+++ app/scripts/controllers/admin.controller.js
@@ -7,9 +7,9 @@ app.controller('AdminCtrl', function ($rootScope, $scope, $http) {
   $scope.loadLockState = function() {
     $http({
       method: 'GET',
-      url: 'http://' + window.location.hostname + ':' + $rootScope.SERVER_PORT + '/api/admin/lockstate',
+      url: 'http://' + window.location.hostname + ':' + $rootScope.SERVER_PORT + '/api/lockstate',
     }).success(function (data) {
-      $scope.lockstate = data;
+      $scope.lockstate = data ? "locked" : "unlocked";
     });
   }
   $scope.loadLockState();
@@ -34,10 +34,10 @@ app.controller('AdminCtrl', function ($rootScope, $scope, $http) {
       method: 'POST',
       url: 'http://'+window.location.hostname+':'+$rootScope.SERVER_PORT+'/api/admin/lock',
     }).success(function(data) {
-      $scope.matchdays = data;
+      $scope.lockstate = "locked";
     });
     $scope.lockstate = 'loading...';
-    $scope.loadLockState();
+    console.log("lock");
   }
 
   $scope.unlock = function() {
@@ -45,9 +45,18 @@ app.controller('AdminCtrl', function ($rootScope, $scope, $http) {
       method: 'POST',
       url: 'http://'+window.location.hostname+':'+$rootScope.SERVER_PORT+'/api/admin/unlock',
     }).success(function(data) {
-      $scope.matchdays = data;
+      $scope.lockstate = "unlocked"
     });
     $scope.lockstate = 'loading...';
-    $scope.loadLockState();
+    console.log("unlock");
+  }
+
+  $scope.load = function(matchday_num) {
+    $http({
+      method: 'POST',
+      url: 'http://'+window.location.hostname+':'+$rootScope.SERVER_PORT+'/api/admin/matchday?id='+matchday_num,
+    }).success(function(data) {
+      console.log(data);
+    });
   }
 });
