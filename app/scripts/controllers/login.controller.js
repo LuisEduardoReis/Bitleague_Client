d29c590287032150faf083ae82222d50d5d79aa0
Inframan <ei12087@fe.up.pt>
2016-05-31 19:23:36 +0100

    :poop: tudo  :poop:

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -2,9 +2,21 @@
 
 app.controller('LoginCtrl', function ($rootScope, $state, $scope, srvAuth) {
 
+
   $scope.login = function () {
     FB.login(function() {}, {scope: 'email,public_profile,user_friends', return_scopes: true});    
     $sessionStorage = service.login;
+    
+
+	if($cookies.getObject("redirect_value"))
+  	{
+  		var redirect_value = $cookies.getObject("redirect_value");
+  		window.location.href = redirect_value.location + redirect_value.id;
+        //$location.path(redirect_value.location + redirect_value.id);
+  	}
+  	else
+  		window.location.href = "/userpage";
   };
+
 
 });
