13cbc7596615d633125214ab3bde7316787591ff
Luis <luiseddie.kr@gmail.com>
2016-06-03 08:48:23 +0100

    save team button working.

--- app/scripts/controllers/team_management.controller.js
+++ app/scripts/controllers/team_management.controller.js
@@ -1,36 +1,7 @@
 'use strict';
 
 app.controller('TeamCtrl', function($rootScope, $scope, $http, $stateParams, srvAuth) {
-  /*
-  $scope.goalkeeper = [
-  { 'id': 'P01', 'title': 'GK1', 'drag': true }
-  ];
-  $scope.defense = [
-  { 'id': 'P02', 'title': 'DF1', 'drag': true },
-  { 'id': 'P03', 'title': 'DF2', 'drag': true },
-  { 'id': 'P04', 'title': 'DF3', 'drag': true },
-  { 'id': 'P05', 'title': 'DF4', 'drag': true }
-  ];
-  $scope.midfield = [
-  { 'id': 'P06', 'title': 'MF1', 'drag': true },
-  { 'id': 'P07', 'title': 'MF2', 'drag': true },
-  { 'id': 'P08', 'title': 'MF3', 'drag': true },
-  { 'id': 'P09', 'title': 'MF4', 'drag': true }
-  ];
-  $scope.forward = [
-  { 'id': 'P10', 'title': 'FW1', 'drag': true },
-  { 'id': 'P11', 'title': 'FW2', 'drag': true }
-  ];
 
-  $scope.bench = [
-  { 'id': 'P12', 'title': 'GK2', 'drag': true },
-  { 'id': 'P13', 'title': 'DF5', 'drag': true },
-  { 'id': 'P14', 'title': 'DF6', 'drag': true },
-  { 'id': 'P15', 'title': 'MF5', 'drag': true },
-  { 'id': 'P16', 'title': 'FW3', 'drag': true },
-  { 'id': 'P17', 'title': 'FW4', 'drag': true },
-  { 'id': 'P18', 'title': 'FW5', 'drag': true }
-  ];*/
   function toDragable(player) { return { 'id': player.data_id, 'title': player.name + " - " + player.positionDescription, 'drag': true };}
 
   $scope.goalkeeper = [];
@@ -49,7 +20,7 @@ app.controller('TeamCtrl', function($rootScope, $scope, $http, $stateParams, srv
     url: 'http://'+window.location.hostname+':'+$rootScope.SERVER_PORT+'/api/team?id='+$stateParams.id,
     headers: {'Authorization': srvAuth.login.token}
   }).success(function(team) {
-    console.log(team);
+    //console.log(team);
 
     if (team.hasTeam) {
       for(var player in team.lineup) {
@@ -72,15 +43,29 @@ app.controller('TeamCtrl', function($rootScope, $scope, $http, $stateParams, srv
       $scope.other.push(toDragable(players[player]));
     }
 
-    /*
-    for(var player in team.players) {
-      $scope.bench.push(toDragable(players[player]));
-    }*/
-
   }).error(function (error) {console.log(error);});
   }).error(function (error) {console.log(error);});
 
+  $scope.saveTeam = function() {
+    var lineup = {}, bench = [];
+    for(var i in $scope.goalkeeper) lineup[$scope.goalkeeper[i].id] = 1;
+    for(var i in $scope.defense) lineup[$scope.defense[i].id] = 2;
+    for(var i in $scope.midfield) lineup[$scope.midfield[i].id] = 3;
+    for(var i in $scope.forward) lineup[$scope.forward[i].id] = 4;
+    for(var i in $scope.bench) bench.push($scope.bench[i].id);
 
+    $http({
+      method: "PUT",
+      url: 'http://'+window.location.hostname+':'+$rootScope.SERVER_PORT+'/api/team',
+      headers: {'Authorization': srvAuth.login.token},
+      data: {
+        league_id: $scope.league_id,
+        lineup: lineup,
+        bench: bench
+      }
+    }).success(function(data) {alert("Saved!");})
+      .error(function (error) {alert(error);});
+  }
 
   $scope.playersOnField = function(){
     return $scope.goalkeeper.length + $scope.defense.length + $scope.midfield.length + $scope.forward.length;
